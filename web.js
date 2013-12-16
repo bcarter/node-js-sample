c9c07c3270124c257a978955f3bc30e2a40a0ed4
bcarter <github@idmsinc.net>
2013-12-15 17:20:57 -0700

    Universe is bigger

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Howdy World!');
+  response.send('Howdy Universe!');
 });
 
 var port = process.env.PORT || 5000;
